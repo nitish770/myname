de53122ef08f5b8a2450cbe97c56a33b61b03cea
Nitish <nitishkumarranjan100@gmail.com>
2022-10-19 12:21:15 +0530

    chnage

---
+++ src/Make.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Kumar()
+{
+    return (
+        <div>
+            <h1>heading one</h1>
+            <h2>this is heading</h2>
+        </div>
+    )
+    
+}
+
+export default Kumar

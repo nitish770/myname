1898eed3275e4f71d900744d60b492842b4ddd18
nitish770 <nitishkumarranjan100@gmail.com>
2022-12-07 21:54:17 +0530

    Hooks function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import {BrowserRouter,Route, Routes} from 'react-router-dom';
-import Home from "./Megha/Home";
-import Navibar from "./Megha/Navibar";
-import About1 from "./Megha/About1";
-import Contact from "./Megha/Contact";
+import Hook from "./Hunks/Hook";
+import "./hooks.css";
+
 
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-    <BrowserRouter>
-     <Navibar/>
-
-
-     <Routes>
-     <Route path="/home" element={<Home/>}/>
-     <Route path="/about" element={<About1/>}/>
-     <Route path="*" element={<Contact/>}/>
-     
-     </Routes>
-
-
-
-     </BrowserRouter>
-    
-   
+   <Hook/>
   </React.StrictMode>
     
 )
